49f79d4a0a01d0aad95108f5c5b8987891dab35e
roman_d <roman.dorodnev@gmail.com>
2015-06-24 02:27:28 +0300

    new commit

--- src/MySaxApp.java
+++ src/MySaxApp.java
@@ -7,17 +7,32 @@ import org.xml.sax.helpers.XMLReaderFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
+class CurrencyRate {
+    String numCode;
+    String charCode;
+    int nominal;
+    String name;
+    BigDecimal value;
+
+}
 
 public class MySaxApp extends DefaultHandler {
     final private static String URL = "http://www.cbr.ru/scripts/XML_daily.asp";
-    String thisElement = "";
+    String curElement = "";
+    static CurrencyRate current;
+    static String code;
 
     public static void main(String args[])
             throws Exception {
+        if (args.length > 0){
+            code = args[0];
+            //TODO: check the right currency numCode
+        }
         XMLReader xr = XMLReaderFactory.createXMLReader();
         MySaxApp handler = new MySaxApp();
         xr.setContentHandler(handler);
@@ -40,34 +55,49 @@ public class MySaxApp extends DefaultHandler {
     }
 
     public void startDocument() {
-        System.out.println("Start document");
+        //System.out.println("Start document");
     }
 
 
     public void endDocument() {
-        System.out.println("End document");
+        //System.out.println("End document");
     }
 
     public void startElement(String uri, String name,
                              String qName, Attributes atts) throws SAXException {
-        thisElement = qName;
+        curElement = qName;
+        if (qName.equals("Valute")) {
+            current = new CurrencyRate();
+        }
+
     }
 
-    public void endElement(String uri, String name, String qName, Attributes atts) throws SAXException {
-        thisElement = "";
-    }
-
-    public void characters(char ch[], int start, int length) {
-
-        String numCode = "";
-
-        if (thisElement.equals("NumCode")) {
-            for (int i = start; i < start + length; i++) {
-                if (ch[i] != 0) {
-                    numCode += ch[i];
-                }
+    public void endElement(String uri, String name, String qName) throws SAXException {
+        if (qName.equals("Valute")) {
+            if (current.numCode.equals(code)){
+                System.out.println(current.nominal + " " + current.name + " " + current.value);
+                //TODO: throw exception to stop parsing if needed currency found
             }
+        }
+        curElement = "";
+    }
 
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        if (curElement.equals("NumCode")) {
+            current.numCode = new String(ch, start, length);
+        }
+        if (curElement.equals("CharCode")) {
+            current.charCode = new String(ch, start, length);
+        }
+        if (curElement.equals("Nominal")) {
+            current.nominal = new Integer(new String(ch, start, length));
+        }
+        if (curElement.equals("Name")) {
+            current.name = new String(ch, start, length);
+        }
+        if (curElement.equals("Value")) {
+            String value = new String(ch, start, length);
+            current.value = new BigDecimal(value.replace(",", "."));
         }
     }
 
